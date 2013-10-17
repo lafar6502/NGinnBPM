02cc7e1f0352868d952217007269293070c2fa3a
lafar6502 <rafal@cogit.pl>
2013-10-17 07:42:24 +0200

    task persistence

--- NGinnBPM.Runtime/Services/SqlTaskInstancePersister.cs
+++ NGinnBPM.Runtime/Services/SqlTaskInstancePersister.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Data.Common;
 using NGinnBPM.Runtime.Tasks;
+using NLog;
 
 namespace NGinnBPM.Runtime.Services
 {
     internal class SqlTaskPersisterSession : TaskPersisterSession
     {
         private SqlSession _ses;
+        private static Logger log = LogManager.GetCurrentClassLogger();
 
         public SqlTaskPersisterSession(ITaskInstanceSerializer ser, SqlSession ses) : base(ser)
         {
@@ -38,19 +40,34 @@ namespace NGinnBPM.Runtime.Services
 
         protected override void WriteRecords(IEnumerable<TaskPersisterSession.TaskHolder> records)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
             using (var cmd = _ses.Connection.CreateCommand())
             {
                 int pcnt = 0;
                 foreach (var th in records)
                 {
+                    int ov = string.IsNullOrEmpty(th.DbVersion) ? 1 : Int32.Parse(th.DbVersion);
                     if (th.State == RecordState.New)
                     {
+                        sb.AppendFormat("insert into TaskInstance (instance_id, task_data, task_type, version) values(@instid{0}, @taskdata{0}, @tasktype{0}, @version{0});\n", pcnt);
                     }
                     else if (th.State == RecordState.Modified)
                     {
+                        sb.AppendFormat("update TaskInstance set task_data=@taskdata{0}, version=@version{0},task_type=@tasktype{0} where instance_id=@instid{0} and version=@oldversion{0};\n", pcnt);
                     }
+                    SqlUtil.AddParameter(cmd, "@instid" + pcnt, th.Deserialized.InstanceId);
+                    SqlUtil.AddParameter(cmd, "@taskdata" + pcnt, th.TaskData);
+                    SqlUtil.AddParameter(cmd, "@version" + pcnt, (ov + 1).ToString());
+                    SqlUtil.AddParameter(cmd, "@tasktype" + pcnt, th.TaskTypeId);
+                    if (th.State == RecordState.Modified)
+                    {
+                        SqlUtil.AddParameter(cmd, "@oldversion" + pcnt, th.DbVersion);
+                    }
+                    pcnt++;
                 }
+                log.Info("Query: {0}", sb.ToString());
+                cmd.CommandText = sb.ToString();
+                cmd.ExecuteNonQuery();
             }
         }
     }
---
+++ NGinnBPM.Runtime/Services/SqlUtil.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace NGinnBPM.Runtime.Services
+{
+    public class SqlUtil
+    {
+        #region SQL
+        /// <summary>
+        /// Add db command parameter
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public static void AddParameter(IDbCommand cmd, string name, string value)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = value == null ? SqlString.Null : value;
+            prm.DbType = DbType.AnsiString;
+            cmd.Parameters.Add(prm);
+        }
+
+        public static void AddParameter(IDbCommand cmd, string name, DateTime? value)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = value.HasValue ? new SqlDateTime(value.Value) : SqlDateTime.Null;
+            prm.DbType = DbType.DateTime;
+            cmd.Parameters.Add(prm);
+        }
+
+        public static void AddParameter(IDbCommand cmd, string name, long val)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = val;
+            prm.DbType = DbType.Int64;
+            cmd.Parameters.Add(prm);
+        }
+
+        public static void AddParameter(IDbCommand cmd, string name, int? value)
+        {
+            IDataParameter para = cmd.CreateParameter();
+            para.DbType = DbType.Int32;
+            para.Value = value.HasValue ? new System.Data.SqlTypes.SqlInt32(value.Value) : new System.Data.SqlTypes.SqlInt32();
+            para.Direction = ParameterDirection.Input;
+            para.ParameterName = name;
+            cmd.Parameters.Add(para);
+        }
+
+        public static void AddParameter(IDbCommand cmd, string name, object val, DbType paramType)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = val;
+            prm.DbType = paramType;
+            cmd.Parameters.Add(prm);
+        }
+
+        public static void AddParameter(IDbCommand cmd, string name, byte[] value)
+        {
+            IDbDataParameter prm = cmd.CreateParameter();
+            prm.ParameterName = name;
+            prm.Value = value;
+            prm.DbType = DbType.Binary;
+            cmd.Parameters.Add(prm);
+        }
+
+        #endregion
+
+        public static bool IsSameDatabaseConnection(string connectionString1, string connectionString2)
+        {
+            if (string.Equals(connectionString1, connectionString2, StringComparison.InvariantCultureIgnoreCase)) return true;
+            SqlConnectionStringBuilder cs1 = new SqlConnectionStringBuilder(connectionString1);
+            SqlConnectionStringBuilder cs2 = new SqlConnectionStringBuilder(connectionString2);
+            if (!string.Equals(cs1.DataSource, cs2.DataSource, StringComparison.InvariantCultureIgnoreCase)) return false;
+            if (!string.Equals(cs1.InitialCatalog, cs2.InitialCatalog, StringComparison.InvariantCultureIgnoreCase)) return false;
+            if (cs1.IntegratedSecurity !=  cs2.IntegratedSecurity) return false;
+            if (!string.Equals(cs1.UserID, cs2.UserID, StringComparison.InvariantCultureIgnoreCase)) return false;
+            //if (!string.Equals(cs1.DataSource, cs2.DataSource, StringComparison.InvariantCultureIgnoreCase) return false;
+            return true;
+        }
+
+        public static DbConnection OpenConnection(string connectionString, string provider)
+        {
+            if (string.IsNullOrEmpty(provider)) provider = "System.Data.SqlClient";
+            var con = DbProviderFactories.GetFactory(provider).CreateConnection();
+            con.ConnectionString = connectionString;
+            con.Open();
+            return con;
+        }
+
+        
+    }
+}

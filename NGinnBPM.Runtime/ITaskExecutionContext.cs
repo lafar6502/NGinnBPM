06772bc37eeaaf28b88f5b2b52f36d4c34c184b2
lafar6502 <rafal.gwizdala@gmail.com>
2015-03-28 09:59:32 +0100

    save

--- NGinnBPM.Runtime/ITaskExecutionContext.cs
+++ NGinnBPM.Runtime/ITaskExecutionContext.cs
@@ -9,7 +9,19 @@ namespace NGinnBPM.Runtime
     public interface ITaskExecutionContext
     {
         void EnableChildTask(TaskExecutionEvents.EnableChildTask msg);
+
         void CancelChildTask(TaskExecutionEvents.CancelTask msg);
+
+        /// <summary>
+        /// starts a child process 
+        /// On completion/failure/cancellation we should get a message 
+        /// </summary>
+        /// <param name="definitionId"></param>
+        /// <param name="inputData"></param>
+        /// <param name="parentTaskInstanceId"></param>
+        /// <param name="parentProcessInstance"></param>
+        /// <returns></returns>
+       // string StartProcess(string definitionId, Dictionary<string, object> inputData, string parentTaskInstanceId, string parentProcessInstance);
 
         /// <summary>
         /// Schedule a task event with future delivery date
